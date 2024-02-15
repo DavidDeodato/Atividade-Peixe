7c80fdde8ee60bee30f0c40cbf7e3705612bbf1b
DavidDeodato <david.nascimento@sou.inteli.edu.br>
2024-02-15 19:56:51 -0300

    Peixe1

---
+++ script/peixe.js
@@ -0,0 +1,65 @@
+//definir as configurações da página
+
+var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    scene: {
+        preload: jogo,
+        create: create,
+        update: update,
+    }
+}
+
+// definindo a variavél do jogo e a do peixe
+
+var game = new Phaser.Game(config);
+var baiacu;
+
+// adicionando as funções necessárias para o jogo funcionar
+
+// carregando as imagens do fundo/cenário
+function jogo() {
+    this.load.image("mar", "assets/bg_azul-claro.png");
+    this.load.image("logo", "assets/logo-inteli_azul.png");
+    this.load.image("concha", "assets/concha.png")
+    this.load.image("crustaceo", "assets/crustaceo.png")
+
+    //atribuindo o baiacu em uma viriável
+
+baiacu = this.load.image("baiacu", "assets/baiacu.png");
+
+
+}
+
+//carregando as imagens na tela 
+
+function create(){
+    this.add.image(400,300, "mar").setScale(2.2);
+    this.add.image(1070, 80, "logo").setScale (0.75);
+    this.add.image(100, 101, "concha").setScale (2.5);
+    this.add.image(200, 400, "crustaceo").setScale (2.0);
+
+    //adicionando o baiacu no jogo
+    baiacu = this.add.image(400, 300, "baiacu").setOrigin(0.5,0.5);
+    baiacu.setFlip(true, false);
+}
+
+//fazendo o peixe se mover na tela  - function (ele atualiza toda vez que tem uma atualização, a todo constante)
+
+function update() {
+
+//configurando para o baiacu poder virar
+    if (this.input.x > baiacu.x) {
+        baiacu.setFlip(true, false);
+    } else {
+        baiacu.setFlip(false, false);
+    }
+
+
+baiacu.x = this.input.x;
+baiacu.y = this.input.y;
+
+}
+
+console.log("Não, nunca programei na vida. Está sendo uma experiência desafiadora, mas espero melhorar")
